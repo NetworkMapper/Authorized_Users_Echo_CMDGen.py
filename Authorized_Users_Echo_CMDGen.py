c85ea7ba4edc0a6de0f8700468345629a972d574
NetworkMapper <176590097+NetworkMapper@users.noreply.github.com>
2024-08-06 14:34:44 +0300

    Update Authorized_Users_Echo_CMDGen.py

--- Authorized_Users_Echo_CMDGen.py
+++ Authorized_Users_Echo_CMDGen.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 import os
 
 print("As long as your public key is saved in your local .ssh as 'id_rsa.pub' this will work!")
-
 
 filepath = os.getenv('HOME') + "/.ssh/id_rsa.pub"
 rsa = open(filepath,"r")
@@ -11,6 +9,5 @@ user = input("What is the user we have to ssh into?")
 
 if user == 'root':
     print(f"\n \necho '{rsa.read()}' > /{user}/.ssh/authorized_keys")
-
 else:
     print(f"\n \necho '{rsa.read()}' > /home/{user}/.ssh/authorized_keys")
